eecec7a53666a67483445c9e850919cb52ec1cea
msi <msi@DESKTOP-8HHQCE2>
2021-08-10 18:21:00 +0900

    Add repeat function

--- src/calendar.java
+++ src/calendar.java
@@ -5,6 +5,7 @@ public class calendar {
 	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public int getmaxDaysOfMonth(int month) {
+
 		return MAX_DAYS[month - 1];
 	}
 
@@ -24,12 +25,20 @@ public class calendar {
 		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		calendar cal = new calendar();
-		System.out.println("최대 일수를 알고싶은 달을 입력하세요.");
-		int month = scanner.nextInt();
+		
+		System.out.println("반복횟수를 입력하세요."); 
+		int repeat = scanner.nextInt();
+		
+		for (int i=0; i < repeat; i++) {
+			System.out.println("최대 일수를 알고싶은 달을 입력하세요.");
+			int month = scanner.nextInt();
 
-		System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getmaxDaysOfMonth(month));
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, cal.getmaxDaysOfMonth(month));
+		}
+		
+		System.out.println("끗~");
 		scanner.close();
-		cal.printSampleCalendar();
+//		cal.printSampleCalendar();
 	}
 
 }
