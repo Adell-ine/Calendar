c5173e817fa2ed7bac7dd7e7f309e4a67b3d00a7
msi <msi@DESKTOP-8HHQCE2>
2021-08-11 16:05:41 +0900

    첫번째 요일을 입력받는 달력

--- src/calendar.java
+++ src/calendar.java
@@ -6,14 +6,13 @@ public class calendar {
 	private static final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
 	public boolean isLeapYear(int year) {
-		
+
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
 			return true;
 		else
-		return false;
+			return false;
 	}
-	
-	
+
 	public int getmaxDaysOfMonth(int year, int month) {
 		if (isLeapYear(year)) {
 			return LEAP_MAX_DAYS[month - 1];
@@ -21,23 +20,49 @@ public class calendar {
 		return MAX_DAYS[month - 1];
 	}
 
-	/**
-	 * - 월을 입력하면 해당 월의 달력을 출력한다. - 달력의 모양은 1단계에서 작성한 모양으로 만든다. - 1일은 일료일로 정해도 무방하다.
-	 * - -1을 입력받기 전까지 반복 입력을 받는다. - 프롬프트를 출력한다.
-	 */
+//	- 1일의 요일을 입력받는다.
+//	- 출력한다.
+//	
+//	년도를 입력하세요.
+//	YEAR> 2021
+//	달을 입력하세요.
+//	MONTH> 8
+//	첫번째 요일을 입력하세요. (SU, MO, WE, TH, FR, SA)
+//	WEEKDAY> WE
 
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.println("--------------------");
+		System.out.println("---------------------");
 
-		int maxDay = getmaxDaysOfMonth(year,month);
+		// print blank sapce
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+		int maxDay = getmaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count< 7) ? count : 0;
+		
+//		if (count < 7) {
+//			delim = count; }
+//		esle {
+//			delim = 0;
+//		}
 
-		for (int i = 1; i <= maxDay; i++) {
+//		print first line
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if (i % 7 == 0)
+		}
+		System.out.println();
+
+//		print from second line to last
+		count++;
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim)
 				System.out.println();
 		}
+		System.out.println();
 		System.out.println();
 //		System.out.println(" 1  2  3  4  5  6  7 ");
 //		System.out.println(" 8  9 10 11 12 13 14");
